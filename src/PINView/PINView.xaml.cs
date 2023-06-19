8ccec04c51fdce4efb2d31116b7e6b26cee9913d
corentin robert <corentin.robert@swiss4.com>
2023-06-19 14:50:41 +0200

    Fix font family

--- src/PINView/PINView.xaml.cs
+++ src/PINView/PINView.xaml.cs
@@ -145,6 +145,7 @@ namespace PINView.Maui
             boxTemplate.CharLabel.FontSize = BoxSize / 2;
             boxTemplate.CharLabel.FontFamily = this.FontFamily;
 
+
             if (DeviceInfo.Platform == DevicePlatform.Android)
             {
                 // Added TapGuesture to all components of the Box so that if we tap anywhere
