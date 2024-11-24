7d1aa1980b89cbd3a9b2cee26cdf5e4dcdb9ce53
MGohil <milan_213@yahoo.co.in>
2024-11-24 19:09:55 +0530

    #15 - BoxStrokeDasArray and BoxStrokeDasOffset properties added

---
+++ src/PINView/BindableProperties/PINView.BoxStrokeDashOffset.cs
@@ -0,0 +1,37 @@
+﻿using PINView.Maui.Helpers;
+
+namespace PINView.Maui
+{
+    public partial class PINView
+    {
+        /// <summary>
+        /// Gets or Sets the StrokeOffset of each PIN Box. Applicable when BoxStrokeDashArray property is set
+        /// </summary>
+        public double BoxStrokeDashOffset
+        {
+            get => (double)GetValue(BoxStrokeDashOffsetProperty);
+            set => SetValue(BoxStrokeDashOffsetProperty, value);
+        }
+
+        public static readonly BindableProperty BoxStrokeDashOffsetProperty =
+          BindableProperty.Create(
+              nameof(BoxStrokeDashOffset),
+              typeof(double),
+              typeof(PINView),
+              1.0,
+              defaultBindingMode: BindingMode.OneWay,
+              propertyChanged: BoxStrokeDashOffsetPropertyChanged);
+
+        private static void BoxStrokeDashOffsetPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = ((PINView)bindable);
+
+            control.PINBoxContainer.Children.ToList().ForEach(x =>
+            {
+                var boxTemplate = (BoxTemplate)x;
+
+                boxTemplate.BoxBorder.StrokeDashOffset = (double)newValue;
+            });
+        }
+    }
+}
